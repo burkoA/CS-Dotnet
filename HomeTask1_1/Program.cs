85396847c92e0fc0d0320ea7079314dace538372
Arsen Burko <arsenburko67@gmail.com>
2024-10-10 18:31:50 +0200

    Homework 1 - solution

---
+++ HomeTask1_1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace HomeTask1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Digits = "0123456789AB";
+
+            Console.WriteLine("Enter first number - ");
+            string firstNumberInput = Console.ReadLine();
+
+            Console.WriteLine("Enter second number - ");
+            string secondNumberInput = Console.ReadLine();
+
+            int firstNumber = int.Parse(firstNumberInput);
+            int secondNumber = int.Parse(secondNumberInput);
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                string calculateValue = "";
+                int currentValue = i;
+                int count = 0;
+
+                while (currentValue > 0)
+                {
+                    int remainder = currentValue % 12;
+
+                    calculateValue = Digits[remainder] + calculateValue;
+                    currentValue /= 12;
+                }
+
+                for (int j = 0; j < calculateValue.Length; j++)
+                {
+                    if (calculateValue[j] == 'A')
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 2)
+                {
+                    Console.WriteLine(i);
+                }
+
+                count = 0;
+                calculateValue = "";
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeTask1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HomeTask1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 9 symbols for ISBN");
+            string isbnInput = Console.ReadLine();
+
+            int weight = 10;
+            int result = 0;
+
+            for (int i = 0; i < isbnInput.Length; i++)
+            {
+                int convertValue = isbnInput[i] - '0';
+
+                result += weight * convertValue;
+
+                weight--;
+            }
+
+            int checkDigit = result % 11;
+
+            if (checkDigit == 10)
+            {
+                Console.WriteLine(isbnInput + "X");
+            }
+            else
+            {
+                Console.WriteLine(isbnInput + checkDigit.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
