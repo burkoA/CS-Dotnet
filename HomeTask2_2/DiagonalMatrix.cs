83561b30d4d765494b321a1db17301a538134992
Arsen Burko <arsenburko67@gmail.com>
2024-10-25 10:42:26 +0200

    Created a separate class for the extension method

--- HomeTask2_2/DiagonalMatrix.cs
+++ HomeTask2_2/DiagonalMatrix.cs
@@ -94,24 +94,3 @@ public class DiagonalMatrix
         return result;
     }
 }
-
-public static class AddMatrices
-{
-    public static DiagonalMatrix Add(this DiagonalMatrix firstMatrix, DiagonalMatrix secondMatrix)
-    {
-        int newSize = Math.Max(firstMatrix._diagonalElement.Length, secondMatrix._diagonalElement.Length);
-
-        int[] resultElements = new int[newSize];
-
-        for (int i = 0; i < newSize; i++)
-        {
-            int firstMatrixElement = i < firstMatrix._diagonalElement.Length ? firstMatrix._diagonalElement[i] : 0;
-
-            int secondMatrixElement = i < secondMatrix._diagonalElement.Length ? secondMatrix._diagonalElement[i] : 0;
-
-            resultElements[i] = firstMatrixElement + secondMatrixElement;
-        }
-
-        return new DiagonalMatrix(resultElements);
-    }
-}
