19319a62ffd9e535c4f922f8bde084a75639c200
Arsen Burko <arsenburko67@gmail.com>
2024-12-14 11:19:05 +0100

    Remove magic numbers

--- HomeTask6/Entities/Author.cs
+++ HomeTask6/Entities/Author.cs
@@ -2,13 +2,15 @@
 {
     public class Author
     {
+        private const int _fullNameMaxLength = 200;
+
         public string FirstName { get; }
         public string LastName { get; }
         public DateTime Birthday { get; }
 
         public Author(string firstName, string lastName, DateTime birthday)
         {
-            if (firstName.Length > 200 || lastName.Length > 200)
+            if (firstName.Length > _fullNameMaxLength || lastName.Length > _fullNameMaxLength)
                 throw new ArgumentException("First name or last name are too long!");
 
             FirstName = firstName;
